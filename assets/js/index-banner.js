afc8612c32ca48525a89e68e9d909a0d889d75c7
BBong119 <59595485+BBong119@users.noreply.github.com>
2020-10-27 13:47:09 +0800

    add url replace js function.

--- assets/js/index-banner.js
+++ assets/js/index-banner.js
@@ -276,3 +276,32 @@ function UsefulRecord(isUseful) {
         feedbackTag.innerHTML = "Thanks!";
     }
 }
+
+function UrlReplace()
+{
+    var curRelativeUrl = (document.URL.split(document.domain)).pop();
+    
+    var tmpExp = new RegExp(/-v[0-9]+.*\//g)
+    var searchAry = tmpExp.exec(curRelativeUrl);
+    if (searchAry != null)
+    {
+        var needFindStr = curRelativeUrl.split(searchAry[0])[0] + "/";
+        var needReplaceStr = curRelativeUrl.split(searchAry[0])[0] + searchAry[0];
+
+        var allHref = $(".content").find("a");
+    
+        for (var i = 0; i < allHref.length; i++)
+        {
+            var hrefVal = allHref[i].href;
+            if (hrefVal.search(needReplaceStr) < 0)
+            {
+                hrefVal = hrefVal.replace(/-v[0-9]+.*\//g,"/");
+                hrefVal = hrefVal.replace(/-v[0-9]+.*.html/g,".html");
+                if (hrefVal.search(needFindStr) > 0)
+                {
+                    allHref[i].href = hrefVal.replace(needFindStr, needReplaceStr);
+                }
+            }
+        }
+    }
+}
